25324009fe54172e44885178f76b0a3117a9139b
Jeffrey Liu <j3ffreyl1u@gmail.com>
2025-09-09 18:45:22 -0400

    Initial Commit

---
+++ Project1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project1
+//
+//  Created by Jeffrey Liu on 9/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by Jeffrey Liu on 9/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
